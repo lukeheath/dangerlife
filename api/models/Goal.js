068423031ced5ff24f6c40ffe661d8a727f4414f
Luke Heath <luke@heath.software>
2021-06-21 15:09:45 -0500

    Update config

---
+++ api/models/Goal.js
@@ -0,0 +1,56 @@
+/**
+ * Goal.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    name: {
+      type: 'string',
+      required: true,
+      description: 'Full representation of the goal\'s name.',
+      maxLength: 200,
+      example: 'Go Skydiving'
+    },
+
+    description: {
+      type: 'string',
+      required: false,
+      description: 'Long form description of the goal.',
+      example: 'I want to go tandem skydiving before my 30th birthday.'
+    },
+
+    deadline: {
+      type: 'number',
+      required: true,
+      description: 'A JS timestamp (epoch ms) representing the moment at which the deadline is reached.',
+      example: 1502844074211
+    },
+
+    reminders: {
+      type: 'boolean',
+      required: true,
+      description: 'Whether reminders are sent for this goal.',
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
